924d8745fb8eb2560f7d7c9dc6e5fa7efb7fdf1b
gabriel.lima <gabrielcostalima21@gmail.com>
2024-10-25 09:49:10 -0300

    fix: ButtonsMiddle do header desaparecendo corrigido

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,12 +1,18 @@
 import { LuArchive } from "react-icons/lu";
 import { Container, LogoDiv } from "./styled";
 import Link from "next/link";
+import { useMain } from "@/context/main";
 
 export default function Logo() {
+    const { setShowMenu } = useMain();
+
+    const handleLogoClick = () => {
+        setShowMenu(true);
+    };
+
     return (
-        
         <Container>
-            <Link href="/">
+            <Link href="/" onClick={handleLogoClick}>
                 <LogoDiv>
                     <LuArchive size={58} color="var(--primaryDarkZaori)"/>
                     <h2>StockPilot</h2>

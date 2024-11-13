79eedab6c8cb5225f42df4d5dea3d407c291a55d
gabriel.lima <gabrielcostalima21@gmail.com>
2024-11-13 08:39:46 -0300

    API

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
+    // baseURL: "http://localhost:8000/api",
     baseURL: "http://192.168.13.27:8005/api",
     headers: {
         'Content-Type': 'application/json'

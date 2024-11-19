a6aaf015747c25011384e1dee4016b515ff90be5
gabriel.lima <gabrielcostalima21@gmail.com>
2024-11-19 10:08:11 -0300

    style: melhora nas cores dos botões de paginação

--- src/components/ItemsHistoryContainer/styled.ts
+++ src/components/ItemsHistoryContainer/styled.ts
@@ -37,16 +37,17 @@ export const PageButton = styled.button<{ $isActive: boolean }>`
     margin: 0 5px;
     padding: 8px 12px;
     border: 1px solid var(--primaryDarkZaori);
-    background-color: ${props => props.$isActive ? 'var(--primaryDarkZaori)' : 'white'};
-    color: ${props => props.$isActive ? 'white' : 'var(--primaryDarkZaori)'};
+    background-color: ${props => props.$isActive ? 'var(--primaryDarkZaori)' : 'var(--background)'};
+    color: ${props => props.$isActive ? 'var(--background)' : 'var(--darkZaori)'};
     cursor: pointer;
     border-radius: 5px;
     transition: all 0.3s ease;
     font-family: var(--font-poppins);
     font-size: 14px;
+    font-weight: 500;
 
     &:hover {
         background-color: var(--primaryDarkZaori);
-        color: white;
+        color: var(--background);
     }
 `;

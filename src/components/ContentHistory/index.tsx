cd678e7286dd3ea5890e059dbdef8298144baac3
Gcld <gabrielcostalima21@gmail.com>
2024-11-27 10:19:05 -0300

    style: adicionando responsividade no ItemHistoryCardContainer

--- src/components/ContentHistory/index.tsx
+++ src/components/ContentHistory/index.tsx
@@ -1,11 +1,12 @@
 import { useMain } from "@/context/main";
 import Filters from "../Filters";
 import ItemsHistoryContainerGrid from "../ItemsHistoryContainerGrid";
-import { Container } from "./styled";
+import { Button, Container, MobileMenu } from "./styled";
 import ItemsHistoryContainer from "../ItemsHistoryContainer";
 import { api } from "@/service/api";
 import { useEffect, useState } from "react";
 import { HistoryItem } from "@/Interfaces/interface";
+import { LuListFilter, LuPlus } from "react-icons/lu";
 
 
 export default function ContentHistory() {
@@ -31,11 +32,24 @@ export default function ContentHistory() {
     
     return (
         <Container>
+            <MobileMenu>
+                <Button>
+                    <LuPlus size={20}/>
+                    <h2>Add Product</h2>
+                </Button>
+                <Button>
+                    <LuListFilter size={20}/>
+                    <h2>Filter</h2>
+                </Button>
+            </MobileMenu>
             <Filters/>
             {isGridView === 'grid' 
                 ? <ItemsHistoryContainerGrid data={movements}/> 
                 : <ItemsHistoryContainer data={movements}/>
             }
+            <div className="mobileGrid">
+                <ItemsHistoryContainerGrid data={movements}/>
+            </div>
         </Container>
     );
 }
--- src/components/ContentHistory/styled.ts
+++ src/components/ContentHistory/styled.ts
@@ -7,4 +7,37 @@ export const Container = styled.div`
     gap: 20px;
     align-content: center;
     justify-content: space-around;
+
+    .mobileGrid{
+        display: none;
+    }
+
+    @media (max-width: 1100px) {
+        flex-direction: column;
+        width: 100%;
+
+        .mobileGrid{
+            display: flex;
+        }
+    }
+`;
+
+export const MobileMenu = styled.div`
+    display: none;
+
+    @media (max-width: 1100px) {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+    }
+`;
+
+export const Button = styled.div`
+    display: flex;
+    background-color: var(--darkZaori);
+    border-radius: 10px;
+    padding: 10px;
+    font-family: var(--font-poppins);
+    font-size: 12px;
+    cursor: pointer;
 `;

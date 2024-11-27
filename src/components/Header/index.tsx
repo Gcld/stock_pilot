12528407f0fe26f3e9e8752c512691f7e0f5ba76
Gcld <gabrielcostalima21@gmail.com>
2024-11-27 10:47:54 -0300

    feat: funcionalidade do burguer menu do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,55 @@
 'use client'
 
 import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ButtonsEnd from "../ButtonsEnd";
 import ButtonsMiddle from "../ButtonsMiddle";
 import Logo from "../Logo";
-import { Container } from "./styled";
+import { Container, MobileMenuOverlay } from "./styled";
 import { useMain } from "@/context/main";
-import { LuMenu } from 'react-icons/lu';
+import { LuMenu, LuX } from 'react-icons/lu';
 
 export default function Header() {
     const { showMenu, setShowMenu } = useMain();
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     const pathname = usePathname();
 
     useEffect(() => {
         setShowMenu(pathname === '/');
+        setMobileMenuOpen(false);
     }, [pathname, setShowMenu]);
 
+    useEffect(() => {
+        if (mobileMenuOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'visible';
+        }
+
+        return () => {
+            document.body.style.overflow = 'visible';
+        };
+    }, [mobileMenuOpen]);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen);
+    };
+
     return (
-        <Container>
-            <Logo />
-            {showMenu && <ButtonsMiddle />}
-            <ButtonsEnd />
-            <div className='mobileMenu'>
-                <LuMenu size={30} onClick={() => setShowMenu(!showMenu)}/>
-            </div>
-        </Container>
+        <>
+            <Container>
+                <Logo />
+                {showMenu && <ButtonsMiddle />}
+                <ButtonsEnd />
+                <div className='mobileMenu' onClick={toggleMobileMenu}>
+                    {mobileMenuOpen ? <LuX size={30}/> : <LuMenu size={30}/>}
+                </div>
+            </Container>
+            {mobileMenuOpen && (
+                <MobileMenuOverlay>
+                    <ButtonsEnd />
+                </MobileMenuOverlay>
+            )}
+        </>
     );
 }
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -7,17 +7,45 @@ export const Container = styled.div`
     background-color: black;
     padding: 20px;
     justify-content: space-between;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
 
-    .mobileMenu{
+    .mobileMenu {
         display: none;
     }
 
     @media (max-width: 780px) {
-        .mobileMenu{
+        .mobileMenu {
             display: flex;
             align-items: center;
             justify-content: center;
-            color: var(--primaryDarkZaori)
+            color: var(--primaryDarkZaori);
+            cursor: pointer;
+            z-index: 1001;
         }
+    }
+`;
+
+export const MobileMenuOverlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.9);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    z-index: 999;
+
+    > div {
+        display: flex !important;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 20px;
     }
 `;
